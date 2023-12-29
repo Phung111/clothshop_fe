3c7e3aba875d50db1ec3ff94210f3ef06044c0ea
Phung Acer <men01225423493@gmail.com>
2023-12-29 23:16:56 +0700

    'update'

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Index from "feature/Index";
+import Utils from "utils/Utils";
+
+export default function App() {
+  return (
+    <>
+      <Utils />
+      <Index />
+    </>
+  );
+}
---
+++ src/utils/Utils.js
@@ -0,0 +1,11 @@
+import FontAwesome from "utils/FontAwesome";
+import Toast from "utils/Toast";
+
+export default function Utils() {
+  return (
+    <>
+      <FontAwesome />
+      <Toast />
+    </>
+  );
+}
